723a4246b21d841545406c5f49f307f7b6e0e674
PA055 <praful.adiga@gmail.com>
2023-04-15 10:39:24 -0400

    prototype 1

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 templates = Jinja2Templates(directory='app/templates')
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,22 @@
 from app import app, templates
 from fastapi import Request, HTTPException
-from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.responses import HTMLResponse, RedirectResponse, Response, StreamingResponse
+import requests_html
+from bs4 import BeautifulSoup as bs
 
 @app.get('/', response_class=HTMLResponse)
 def index(request: Request):
     return templates.TemplateResponse('main.html', {'request': request})
+
+
+@app.get('/main/{url:path}', response_class=Response)
+async def proxy(url: str):
+    session = requests_html.AsyncHTMLSession()
+    r = await session.get(url)
+    if any(url.endswith(f'.{ext}') for ext in ['png', 'jpg', 'jpeg', 'gif', 'tif', 'tiff', 'svg', 'bmp']):
+        return Response(content=r.content, media_type='image')
+    imgs = r.html.find('img')
+    for img in imgs:
+        img.src = '/main/' + url + '/'
+    await r.html.arender()
+    return r.text
