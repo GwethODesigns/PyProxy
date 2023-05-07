f89e714d4a3d5027d339cccb441dc2547ab5c019
PA055 <praful.adiga@gmail.com>
2023-05-07 09:10:57 -0400

    work on proxy

--- app/helper.py
+++ app/helper.py
@@ -1,4 +1,7 @@
 from bs4 import BeautifulSoup
+from fastapi.responses import HTMLResponse, RedirectResponse, Response, FileResponse, StreamingResponse
+from PIL import Image
+import requests_html, requests, io
 
 def find_list_resources (tag: str, attribute: str, soup: BeautifulSoup):
    list = {}
@@ -11,6 +14,75 @@ def find_list_resources (tag: str, attribute: str, soup: BeautifulSoup):
     
    return list
 
+
+def get_host(url: str) -> str:
+
+    if url.startswith('http://'):
+        url = url if url.startswith('http://www.') else ('http://www.' + url[7:])
+
+    if url.startswith('https://'):
+        url = url if url.startswith('https://www.') else ('https://www.' + url[8:])
+        
+    url = url if url[-1] != "/" else url[:-1]
+
+    url = '/'.join(url.split('/')[:3])
+
+    return url
+
+
+def processSite(soup: BeautifulSoup, url: str):
+    # IMG SRC
+    imgs = find_list_resources('img', 'src', soup)
+    for img, _ in imgs.items():
+        if img["src"].startswith("data:"):
+            continue
+
+        if img["src"].startswith('https://') or img["src"].startswith('http://'):
+            img["src"] = f'/main/_img/{img["src"]}'
+        else:
+            img["src"] = f'/main/_img/{get_host(url)}{img["src"]}'
+
+    # IMG SRCSET
+    for item in soup.find_all('img'):
+        if item.get('srcset') != None:
+            srcset = item['srcset'].split(',')
+            for i, src in enumerate(srcset):
+                if src.startswith('https://') or src.startswith('http://'):
+                    srcset[i] = f'/main/_img/{src}'
+                else:
+                    srcset[i] = f'/main/_img/{get_host(url)}{src}'
+            item['srcset'] = ', '.join(srcset)
+
+    # JS SRC
+    scripts = find_list_resources('script', 'src', soup)
+    for script, _ in scripts.items():
+        if script["src"].startswith('https://') or script["src"].startswith('http://'):
+            script["src"] = f'/main/_js/{script["src"]}'
+        else:
+            script["src"] = f'/main/_js/{get_host(url)}{script["src"]}'
+
+    # LINK HREF
+    links = find_list_resources('link', 'href', soup)
+    for link, _ in links.items():
+
+        if link.get('as') == 'script' and link['href'].endswith('.js'):
+            linkType = 'js'
+        elif link['href'].endswith('.css') and 'stylesheet' in link.get('rel'):
+            linkType = 'css'
+        elif 'icon' in link.get('rel'):
+            linkType = 'img'
+        else:
+            linkType = 'txt'
+        
+        if link["href"].startswith('https://') or link["href"].startswith('http://'):
+            link["href"] = f'/main/_{linkType}/{link["href"]}'
+        else:
+            link["href"] = f'/main/_{linkType}/{get_host(url)}{link["href"]}'
+
+    return soup
+    
+
+
 def get_sources(soup: BeautifulSoup):
     srcs = {}
     srcs.update(find_list_resources('img',"src",soup))
@@ -22,18 +94,21 @@ def get_sources(soup: BeautifulSoup):
     srcs.update(find_list_resources("embed","src",soup))
     srcs.update(find_list_resources("object","data",soup))
     srcs.update(find_list_resources("source","src",soup))
+    srcs.update(find_list_resources("a","href",soup))
 
     return srcs
 
 
-def sterilize_url(url: str) -> str:
 
-    if url.startswith('http://'):
-        url = url if url.startswith('http://www.') else ('http://www.' + url[7:])
+def CheckImages(url, session):
+    if url.endswith('.png'):
+        im = Image.open(requests.get(url, stream=True).raw)
+        img_byte_arr = io.BytesIO()
+        im.save(img_byte_arr, format='PNG')
+        img_byte_arr = img_byte_arr.getvalue()
+        return Response(content=img_byte_arr, media_type="image/png")
 
-    if url.startswith('https://'):
-        url = url if url.startswith('https://www.') else ('https://www.' + url[8:])
-        
-    url = url if url[-1] != "/" else url[:-1]
-
-    return url
+    if url.endswith('.gif'):
+        img_raw = requests.get(url, stream=True).content
+        byte_io = io.BytesIO(img_raw)
+        return StreamingResponse(byte_io, media_type='image/gif')
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from app import app, templates
-from app.helper import get_sources, sterilize_url
+from app.helper import *
 from fastapi import Request, HTTPException
-from fastapi.responses import HTMLResponse, RedirectResponse, Response, FileResponse
+from fastapi.responses import HTMLResponse, RedirectResponse, Response, FileResponse, StreamingResponse
 from PIL import Image
 import requests_html, requests, io
 from bs4 import BeautifulSoup as bs
@@ -10,39 +10,46 @@ from bs4 import BeautifulSoup as bs
 def index(request: Request):
     return templates.TemplateResponse('main.html', {'request': request})
 
-@app.get('/main')
+@app.get('/main/_rd', response_class=RedirectResponse)
 def proxyRedirect(url: str = 'https://www.google.com'):
+    url = url if url != '' else 'https://www.google.com'
     return RedirectResponse(f'/main/{url}')
+
+@app.get('/main/_img/{url:path}', response_class=Response)
+def ImageProxy(url: str):
+    return CheckImages(url, requests_html.AsyncHTMLSession()) or RedirectResponse(f'/main/{url}')
+
+@app.get('/main/_js/{url:path}')
+def JavaScriptProxy(url: str):
+    r = requests.get(url)
+    return Response(r.content)
+
+@app.get('/main/_css/{url:path}')
+def CSSProxy(url: str):
+    r = requests.get(url)
+    return Response(r.content)
+
+@app.get('/main/_txt/{url:path}')
+def JavaScriptProxy(url: str):
+    r = requests.get(url)
+    return Response(r.content)
 
 @app.get('/main/{url:path}', response_class=Response)
 async def proxy(url: str):
     session = requests_html.AsyncHTMLSession()
 
-    if url.endswith('.png'):
-        im = Image.open(requests.get(url, stream=True).raw)
-        img_byte_arr = io.BytesIO()
-        im.save(img_byte_arr, format='PNG')
-        img_byte_arr = img_byte_arr.getvalue()
-        return Response(content=img_byte_arr, media_type="image/png")
+    img = CheckImages(url, session)
+    if img:
+        return img
+
+    # if url.endswith
 
     r = await session.get(url)
 
     await r.html.arender()
     soup = bs(r.text, "html.parser")
+
     
-    srcs = get_sources(soup)
+    soup = processSite(soup, url)
 
-    for tag, attr in srcs.items():
-        if tag[attr].startswith("data:"):
-            continue
-
-        if tag[attr].startswith('https://') or tag[attr].startswith('http://'):
-            tag[attr] = f'/main/{tag[attr]}'
-        else:
-            tag[attr] = f'/main/{sterilize_url(url)}{tag[attr]}'
-
-    for item in soup.find_all('img'):
-        if item.get('srcset') != None:
-            item['srcset'] = ', '.join(['/main/' + sterilize_url(url) + i for i in item['srcset'].split(', ')])
-    
     return soup.prettify()
